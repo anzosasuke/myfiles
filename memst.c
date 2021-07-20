5b5db94e67005c77c1d7ea5b3ce0bd6b7d115982
anzosasuke <33804183+anzosasuke@users.noreply.github.com>
2021-07-20 10:16:21 -0500

    Add files via upload

---
+++ memst.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+// memset() is used to fill a block of memory with a particular value.
+//memset is defined in string.h
+
+// The compiler is generally allowed to remove code
+// that does not have any effect, according to the abstract machine of the C language.
+// This means that if you have a buffer that contains sensitive data (for instance passwords),
+// calling memset on the buffer before releasing the memory will probably be optimized away.
+
+
+// size_t is an unsigned integral data type which is defined in various header files such as:
+ // <stddef.h>, <stdio.h>, <stdlib.h>, <string.h>, < time .h>, <wchar.h>
+ // It's a type which is used to represent the
+// size of objects in bytes and is therefore used as the return type by the sizeof operator.
+
+void f(char *password, size_t bufferSize)
+{
+  char localToken[256];
+  char localBuffer[256];
+  // init(localToken, password);
+  memset(password, ' ', strlen(password)); // Noncompliant, password is about to be freed
+  memset(localToken, ' ', strlen(localBuffer)); // Noncompliant, localToken is about to go out of scope
+  free(password);
+}
+// ptr ==> Starting address of memory to be filled
+// x   ==> Value to be filled
+// n   ==> Number of bytes to be filled starting
+//         from ptr to be filled
+// void *memset(void *ptr, int x, size_t n);
+
+int main()
+{
+char *password;
+password = (char*)malloc(10);//typecasting 
+printf("enter your password: ");
+scanf("%s", password);
+printf("%s", password);
+f(password, 10);
+printf("%s\n",password );
+return 0;
+}
