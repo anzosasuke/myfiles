28322c947a4f67c41d561d25573e4436063ec1ec
anzosasuke <33804183+anzosasuke@users.noreply.github.com>
2022-06-10 09:48:27 -0500

    Update memst.c

--- memst.c
+++ memst.c
@@ -15,46 +15,55 @@
  // It's a type which is used to represent the
 // size of objects in bytes and is therefore used as the return type by the sizeof operator.
 
+// memset() is used to fill a block of memory with a particular value.
+//memset is defined in string.h
 
-undefined8 main(void)
+// The compiler is generally allowed to remove code
+// that does not have any effect, according to the abstract machine of the C language.
+// This means that if you have a buffer that contains sensitive data (for instance passwords),
+// calling memset on the buffer before releasing the memory will probably be optimized away.
 
+
+// size_t is an unsigned integral data type which is defined in various header files such as:
+ // <stddef.h>, <stdio.h>, <stdlib.h>, <string.h>, < time .h>, <wchar.h>
+ // It's a type which is used to represent the
+// size of objects in bytes and is therefore used as the return type by the sizeof operator.
+
+void f(char *password, size_t bufferSize, char *token1, size_t buffersz)
 {
-  void *pvVar1;
-  char *__s;
+
+  char localToken[256];
+  char localBuffer[256];
+  // init(localToken, password);
+  memset(password, ' ', strlen(password));
+  password = token1; // Noncompliant, password is about to be freed
+  memset(token1, ' ', strlen(token1));
   
-  pvVar1 = malloc(10);
-  __s = (char *)malloc(10);
-  printf("enter your password: ");
-  __isoc99_scanf(&DAT_0010201a,__s);
-  printf("%s",__s);
-  f(__s,10,pvVar1,10);
-  puts(__s);
-  return 0;
+  memset(localToken, ' ', strlen(localBuffer)); // Noncompliant, localToken is about to go out of scope
+  free(password);
+  free(token1);
+}
+// ptr ==> Starting address of memory to be filled
+// x   ==> Value to be filled
+// n   ==> Number of bytes to be filled starting
+//         from ptr to be filled
+// void *memset(void *ptr, int x, size_t n);
+
+int main()
+{
+char *password;
+char *token1;
+token1 = (char*)malloc(10);
+password = (char*)malloc(10);//typecasting 
+printf("enter your password: ");
+
+scanf("%s", password);
+printf("%s", password);
+
+f(password, 10, token1, 10);
+printf("%s\n",password );
+return 0;
 }
 
-void f(char *param_1,undefined8 param_2,char *param_3)
-
-{
-  size_t sVar1;
-  long in_FS_OFFSET;
-  undefined local_218 [256];
-  char local_118 [264];
-  long local_10;
-  
-  local_10 = *(long *)(in_FS_OFFSET + 0x28);
-  sVar1 = strlen(param_1);
-  memset(param_1,0x20,sVar1);
-  sVar1 = strlen(param_3);
-  memset(param_3,0x20,sVar1);
-  sVar1 = strlen(local_118);
-  memset(local_218,0x20,sVar1);
-  free(param_3);
-  free(param_3);
-  if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
-                    /* WARNING: Subroutine does not return */
-    __stack_chk_fail();
-  }
-  return;
-}
 
 
